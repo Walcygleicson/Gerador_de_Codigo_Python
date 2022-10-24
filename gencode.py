adedf7cf8036b531ab685e5f20a1a89c7acc857f
Walcygleicson <113854464+Walcygleicson@users.noreply.github.com>
2022-10-23 22:19:50 -0300

    revisão da docstring

--- gencode.py
+++ gencode.py
@@ -4,19 +4,36 @@ def gencode (sequencia, cap='mai'):
     import string
 
     '''
-    Gera uma sequência personalizável de números e letras aleatória.
+    lang = pt-br
+    Gera uma sequência personalizável de números e letras aleatórias.
 
-    Params:
-        sequencia - introduza, entre aspas, a quantidade de letras e/ou números que deseja gerar, sendo que 'l' para letras e 'n' para números e em seguida, sem espaços, digite a quantiade de caractere da respectiva letra ou número que deseja gerar separando cada conjunto com o sinal de '/'.
-        Ex 1: 'l2/n4'
-        Gera uma sequência de 6 dígitos, sendo 2 letras e 4 números - WR0933
-        Ex 2: 'n2/l4/n1' = 51PGRT9
-        Para gerar aleatoriamente use 'aleat' como argumento deste param.
+    Possui 2 parâmetros, sendo o primeiro obrigatório e o segundo, se não declarado, gera letras maiúsculas por padrão. Recebem strings como argumentos, sendo eles:
 
-        cap - captaliza as sequências de letras, sendo:
-            cap ='mai' para tornar todas as letras maiúsculas (padrão).
-            cap ='min' para tornar todas letras minúsculas.
-            cap ='aleat' para tornar algumas letras maisúsculas e minúsculas aleatoriamente.
+    --> sequencia:
+        Recebe 3 tipos de argumentos, sendo eles:
+        * ('n0/l0/n0...'):
+            Escreva o padrão e a quantidade de letras e/ou números que deseja gerar, sendo que 'l' para letras e 'n' para números e em seguida, sem espaços, digite a quantiade de caractere da respectiva letra ou número que deseja gerar separando cada conjunto com o sinal de '/'.
+            Ex 1: ('l2/n4') Gera uma sequência de 6 caracteres, sendo 2 letras e 4 números = WR0933
+            Ex 2: ('n2/l4/n1') Gera uma sequência de 7 caracteres, sendo 2 letras, 4 números e 1 letra = 51PGRT9
+        * ('aleat'):
+            Gera uma sequência de letras e números totalmente aleatória com tamanho que varia entre mínimo de 4 caracteres e máximo de 9 caracteres.
+        * ('aleat/0):
+            Gera uma sequência totalmente aleatória mas seu tamanho pode ser definido após a '/'.
+            Ex: ('aleat/3') Gera uma sequência aleatória de 3 caracteres = A12
+
+    --> cap:
+        Captaliza as sequências de letras. Recebe 3 argumentos, sendo eles:
+            * (cap ='mai'):
+                Para tornar todas as letras maiúsculas (padrão).
+            * (cap ='min'):
+                Para tornar todas letras minúsculas.
+            * (cap ='aleat')
+                Para tornar algumas letras maisúsculas e minúsculas aleatoriamente.
+
+    Exemplos:
+        gencode ('l3/n2/l1','aleat'): = rAt38x
+        gencode ('aleat/6', 'min'): = 2fn71b
+        gencode ('aleat'): = ASV80M2
     '''
 
     resultado = str() # Recebe a lista de conjuntos de sequências unidos em uma única string.
@@ -26,7 +43,7 @@ def gencode (sequencia, cap='mai'):
     ####### Sequencia Personalizada (l0/n0...)
     if 'aleat' not in argumentos_list: # Se o argumento 'aleat' não for declarado...
         for arg in argumentos_list:
-            quantidade = int(arg[1:])  # Remove a letra (l ou n) do conjunto, deixando apenas os números. Será usado para definir a quantidade de letras ou números que será gerado.
+            quantidade = int(arg[1:])  # Remove a letra (l ou n) do conjunto, deixando apenas os números. Será usado para definir a quantidade de letras ou números que será gerado mais a frente.
 
             # Gerando as letras conforme a quantidade passada no conjunto 'l'
             if 'l' in arg:
@@ -36,7 +53,7 @@ def gencode (sequencia, cap='mai'):
                 for _ in range(quantidade):
                     letra = choice(string.ascii_letters)
 
-                    # Configuração do argumento 'aleat' do parâmetro 'cap'.
+                    # Configuração do argumento 'aleat' do parâmetro cap.
                     if cap == 'aleat':
                         cap_aleat1 = randint(0, 1)
                         if cap_aleat1 <= 0:
@@ -61,19 +78,20 @@ def gencode (sequencia, cap='mai'):
                 conjuntos.append(n_sequencia[:])
                 n_list.clear()
 
-        resultado = resultado.join(conjuntos)
+        resultado = resultado.join(conjuntos) # Juntando os conjuntos l e n para formar uma única string de código.
 
-    ####### Sequencias Aleatórias (aleat e aleat/0)
+    ####### Sequências Aleatórias (aleat e aleat/0)
     elif 'aleat' in argumentos_list:
         aleat_sequencia = str()
         aleat_list = list()
 
-        # Gerando sequencia totalmente aleatória (aleat)
+        # Gerando a sequência totalmente aleatória (aleat)
         if argumentos_list[0] == 'aleat' and len(argumentos_list) == 1:
             tamanho_minmax = randint(4, 9) # O tamanho do código gerado de forma totalmente aleatória terá, por padrão, o tamanho min de 4 caracteres e o tamanho máx de 9 caracteres. Os tamanhos serão escolhido aqui.
             for _ in range(tamanho_minmax):
                 escolha = choice([choice(string.ascii_letters), str(randint(0, 9))])
 
+                # Configuração do argumento 'aleat' do parâmetro cap.
                 if cap == 'aleat' and escolha == escolha[0]:
                         cap_aleat2 = randint(0, 1)
                         if cap_aleat2 <= 0:
@@ -90,6 +108,7 @@ def gencode (sequencia, cap='mai'):
             for _ in range(int(argumentos_list[1])):
                 escolha2 = choice([choice(string.ascii_letters.upper()), str(randint(0, 9))])
 
+                # Configuração do argumento 'aleat' do parâmetro cap.
                 if cap == 'aleat' and escolha2 == escolha2[0]:
                         cap_aleat3 = randint(0, 1)
                         if cap_aleat3 <= 0:
@@ -101,26 +120,25 @@ def gencode (sequencia, cap='mai'):
             aleat_sequencia = aleat_sequencia.join(aleat_list)
         resultado = aleat_sequencia
 
-    ##### Configuração dos argumentos do parâmetro 'cap'.
+    ##### Configuração dos argumentos 'mai' e 'min' do parâmetro cap.
     if cap == 'mai':
         resultado = resultado.upper()
     elif cap == 'min':
         resultado = resultado.lower()
 
-
     return resultado
 
-
-
-
-
+#
+#
+##### TESTANDO A FUNÇÃO
+print('Sequencia com padrão personalizado. Letras minúsculas')
 print(gencode('n2/l4','min'))
-print(gencode('n2/l4'))
-print(gencode('l9','min'))
-print(gencode('aleat','aleat'))
+print('Sequencia com padrão personalizado. Letras miúsculas padrão')
+print(gencode('n2/l1/n1/l2'))
+print('Sequencia com padrão personalizado. Letras aleatórias')
+print(gencode('l5','aleat'))
+print('Sequencia com padrão aleatório. Letras maiúsculas padrão')
 print(gencode('aleat'))
+print('Sequencia com padrão aleatório e tamanho definido. Letras aleatórias')
 print(gencode('aleat/5', 'aleat'))
-
-x = 'l3/n1'
-
-print(gencode(x))
+print('Obrigado ;-) ')
